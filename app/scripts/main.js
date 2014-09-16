c0d49ae325ab34329084938c075f2c4e870d0b9f
slkennedy <slkennedy@gmail.com>
2014-09-16 01:20:14 -0400

    Completed Delete Call, Started Put Call - Currently Not Working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,7 +6,9 @@ $("form").submit(function(event) {
         type: "POST",
         dataType: "json",
         data: dataArray
-    });
+    }).done (function(refresh){
+		location.reload();
+	});
 });
 
 function reusableTemplate(templateId, container, model) {
@@ -36,14 +38,54 @@ $('.viewbutton').on('click', function() {
 	});
 });
 
-$(document).on('click', '.delete', function(event) {
+$(document).on('click', '.delete', function(another) {
     event.preventDefault();
     var idname = $(this).attr("id").slice(2);
-    console.log (idname);
-    $.ajax("http://tiny-pizza-server.herokuapp.com/collections/volunteers"+idname, {
+    $.ajax("http://tiny-pizza-server.herokuapp.com/collections/volunteers/"+idname, {
         type: "DELETE"
-    });
-}).done(function(message){
-	alert("Deletion Completed");
-}); 
+    }).done(function(success){
+     $('tr').remove('.rows');
+ 	 alert("Deletion Completed");
+	}); 
+});
 
+// $(document).on('click', '.update', function(updating){
+// 	event.preventDefault();
+// 	var update = $(this).attr("id").slice(2);
+// $.ajax("http://tiny-pizza-server.herokuapp.com/collections/volunteers/"+update, {
+//       type='GET',
+//       dataType='json'
+//     }).done(function(update){
+
+//     	var tableData = {
+// 	        firstName: vol.firstname,
+// 	        lastName: vol.lastname,
+// 	        phone: vol.phone,
+// 	        email: vol.email,
+// 	        job: vol.job,
+// 	        vol_ID: vol._id
+// 	    };
+	    
+//       $('#updatefirst').val('vol.firstname')
+// 	  $('#updatelast').val('vol.lastname')
+// 	  $('#updatephone').val('vol.phone')
+// 	  $('#updateemail').val('vol.email')
+// 	  $('#updatejob').val('vol.job')
+//     })
+
+//  $("form").submit(function(update){
+// 	var updateData = $(this).serializeArray();
+//     event.preventDefault();
+//    $.ajax("http://tiny-pizza-server.herokuapp.com/collections/volunteers/"+update, {
+//         type: "PUT",
+//         dataType: "json",
+//         data: updateData
+//     }).done (function(refresh){
+// 		alert("Update Complete");
+// 	});
+// });
+//  })
+
+//   });
+
+// });
